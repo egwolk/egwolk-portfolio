070337ebf33c88488c92c97dedafcd72e3498e0d
egwolk <erincovacha@gmail.com>
2026-02-15 22:55:39 +0800

    fix: correct dark mode styling for FaMoon icon in ThemeToggle component

--- src/app/theme-toggle.tsx
+++ src/app/theme-toggle.tsx
@@ -8,7 +8,7 @@ export function ThemeToggle() {
     return(
         <Button variant="outline" size="icon" className="rounded-full" onClick={() => setTheme(theme === "light" ? "dark" : "light")}>
             <FaSun className="absolute h-10 w-10 rotate-0 scale-100 dark:-rotate-90 dark:scale-0"></FaSun>
-            <FaMoon className="absolute h-10 w-10 rotate-90 scale-0 dark:-rotate-0 dark:scale-100"></FaMoon>
+            <FaMoon className="absolute h-10 w-10 rotate-90 scale-0 dark:rotate-0 dark:scale-100"></FaMoon>
         </Button>
     )
 }
