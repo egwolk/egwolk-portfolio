b51d928398d50319a1048183d8794152228c85da
egwolk <erincovacha@gmail.com>
2026-02-17 16:38:37 +0800

    style: enhance BottomNav component styling and structure

--- src/components/ui/bottom-nav.tsx
+++ src/components/ui/bottom-nav.tsx
@@ -13,11 +13,13 @@ interface BottomNavProps {
 const BottomNav = ({ items }: BottomNavProps) => {
   return (
     <nav className='fixed bottom-0 left-0 right-0'>
-        <ul className='bg-white text-blue-600 grid grid-cols-4'>
+        <ul className='bg-black dark:bg-white text:black dark:text-white grid grid-cols-4 gap-1 m-2 text-center rounded-4xl align-items-center justify-items-center px-2 transition-all duration-300'>
             {items.map((item, index) => (
                 <li key={index}>
                     <Link href={item.href}>
-                        {item.label}
+                        <div className=" bg-white dark:bg-black rounded-lg min-h-15 min-w-15 m-3 max-h-20 max-w-20 transition-all duration-300">
+                            {item.label}
+                        </div>
                     </Link>
                 </li>
             ))}
